3faef3080bd7798ded9bb3bc2d97cf58fa11ef36
Ohazulike Stanley <sohazulike@gmail.com>
2025-09-19 02:31:55 -0700

    updated users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -5,23 +5,23 @@ import UserCard from "@/components/common/UserCard";
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const users = await response.json()
+  const posts = await response.json()
 
   return {
     props: {
-      users
+      posts
     }
   }
 }
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
   return (
     <div>
         <Header />
       <h1 className="text-4xl font-bold">User Profiles</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {users.map(user => (
-        <UserCard key={user.id} {...user} />
+      {posts.map(post => (
+        <UserCard key={post.id} {...post} />
       ))}
       </div>
     </div>
